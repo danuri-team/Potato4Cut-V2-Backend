c0a5946d2c72e666d17a25913dd837741cfa45c9
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-11-21 09:35:25 +0900

    feat :: 환경변수로 firebase key 가져오기

--- src/main/java/com/potato/cut4/common/config/FirebaseConfig.java
+++ src/main/java/com/potato/cut4/common/config/FirebaseConfig.java
@@ -3,34 +3,48 @@ package com.potato.cut4.common.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.IOException;
-import java.io.InputStream;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
+  @Value("${firebase.credentials.key}")
+  private String firebaseCredentialsBase64;
+
   @PostConstruct
   public void initialize() {
     try {
-      InputStream serviceAccount = new ClassPathResource(
-          "google-credentials.json").getInputStream();
-
-      FirebaseOptions options = FirebaseOptions.builder()
-          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-          .build();
-
       if (FirebaseApp.getApps().isEmpty()) {
+
+        byte[] jsonBytes = Base64.getDecoder().decode(firebaseCredentialsBase64);
+
+        String jsonString = new String(jsonBytes, StandardCharsets.UTF_8);
+        JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+        String projectId = jsonObject.get("project_id").getAsString();
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+            new ByteArrayInputStream(jsonBytes)
+        );
+
+        FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(credentials)
+            .setProjectId(projectId)
+            .build();
+
         FirebaseApp.initializeApp(options);
-        log.info("Firebase Admin SDK initialized successfully");
       }
-    } catch (IOException e) {
+    } catch (Exception e) {
       log.error("Failed to initialize Firebase Admin SDK", e);
-      throw new RuntimeException("Failed to initialize Firebase", e);
+      throw new RuntimeException("Failed to initialize Firebase Admin SDK", e);
     }
   }
 }
